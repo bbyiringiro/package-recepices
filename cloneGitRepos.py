45e1e32abdb4220de69b88366f134169735a8137
Billy Byiringiro <bbyiring@rational.is.ed.ac.uk>
2019-04-03 11:12:00 +0100

    adding repo update push and pull

--- cloneGitRepos.py
+++ cloneGitRepos.py
@@ -104,7 +104,9 @@ def pushLocalRepo(repo_path, name, update=False):
             origin = repo.remotes.origin
         else:
             remote = git.remote.Remote(repo, 'origin')
-            remote.set_url(pushUrl)
+            # remote.set_url(pushUrl)
+            remote.create(repo, 'upstream',pushUrl)
+
             origin = repo.remotes.origin
         origin.push('master')
         print('success')
