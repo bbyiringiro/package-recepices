abb4a2b902d772d5a96f556f19a2d0bd50b0874e
Billy Byiringiro <bbyiring@rational.is.ed.ac.uk>
2019-04-03 11:27:51 +0100

    adding repo update feature

--- cloneGitRepos.py
+++ cloneGitRepos.py
@@ -104,8 +104,8 @@ def pushLocalRepo(repo_path, name, update=False):
             origin = repo.remotes.origin
         else:
             remote = git.remote.Remote(repo, 'origin')
-            # remote.set_url(pushUrl)
-            remote.create(repo, 'upstream',pushUrl)
+            remote.add_url(pushUrl)
+            # remote.create(repo, 'upstream',pushUrl)
 
             origin = repo.remotes.origin
         origin.push('master')
